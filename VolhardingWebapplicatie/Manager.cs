53a126ec2872621208d86c2184af4f4faa3c4e27
MartijnvDijk <martijn.van.dijk@live.nl>
2014-05-22 10:08:20 +0200

    Klassen + Enum aangemaakt
    De klasses en het enum is aangemaakt.

---
+++ VolhardingWebapplicatie/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VolhardingWebapplicatie
+{
+    enum Spullen
+    {
+        Bal,
+        Pion,
+        Shirt
+    }
+    public class Manager
+    {
+    }
+}
